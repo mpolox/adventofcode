e090c58eebcf8ab0e541eb208a49252dec55429a
Marcopolo Ramos <mpolox@gmail.com>
2021-12-23 22:23:21 -0600

    Part2

--- src/functions/common/matrix.js
+++ src/functions/common/matrix.js
@@ -10,4 +10,17 @@ const newMatrix = (a,b) => {
   return matrix;
 }
 
-export {newMatrix}
+const newMatrixInt = (a,b) => {
+  let matrix = [];
+  for (let y = 0; y < a; y++) {
+    let vectorX = [];
+    for (let x = 0; x < b; x++) {
+      vectorX[x] = 0;      
+    }      
+    matrix[y] = vectorX;
+  }
+  return matrix;
+}
+
+
+export {newMatrix, newMatrixInt}
