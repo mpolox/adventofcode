2118b46cc76a890e34dfba1dcd1f4e68e5146a07
Marcopolo Ramos <mpolox@gmail.com>
2022-01-02 13:31:27 -0600

    2015 Day 8, part 2

--- src/functions/2015/day8.js
+++ src/functions/2015/day8.js
@@ -2,9 +2,6 @@ const Y2015_Day8 = (data) => {
 
   let codeLength = 0;
   let strLenght = 0;
-  let allStr = "";
-
-  const hexCode = "\\x[a-f0-9]{2}"
 
   const Part1 = () => {
     for (let i = 0; i < data.length; i++) {      
@@ -13,19 +10,25 @@ const Y2015_Day8 = (data) => {
       element = element.replace(/\\x[a-f0-9]{2}/g,"X"); // xFF -> X
       element = element.replace(/\\\\/g,"Z");           // \\  -> /      
       element = element.replace(/\\"/g,"Y");            // \"  -> '
-      element = element.replace(/\"/g,"");              // "   -> EMPTY
-
+      element = element.replace(/"/g,"");              // "   -> EMPTY
       strLenght += element.length;
-      allStr += element;
     }
-    console.log("T", codeLength);
-    console.log("C", strLenght);
-    console.log(allStr);
     return(codeLength - strLenght);
   }
 
   const Part2 = () => {
-    return("TBD");
+    codeLength = 0;
+    strLenght = 0;
+    for (let i = 0; i < data.length; i++) {      
+      let element = data[i];
+      codeLength += element.length;      
+      element = "X" + element.substring(1,element.length-1) + "X";
+      element = element.replace(/\\/g,"ZZ");           // \  -> Z
+      element = element.replace(/"/g,"WW");            // "  -> W
+      element = element.replace(/X/g,"YYY");           // "  -> W
+      strLenght += element.length;
+    }    
+    return(strLenght - codeLength);
   }
   
   const response = {
